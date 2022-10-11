a2e5f278737ba662b25a54e89ee4e94eafd76d57
carloscl2001 <carloscorteslora@gmail.com>
2022-10-11 09:11:21 +0200

    hhhh

--- PRACTICAS/P1/nAleatorio.java
+++ PRACTICAS/P1/nAleatorio.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-import java.util.*;
+import java.util.Scanner;//es para usar la clase Scanner de la bibilioteca util
+import java.util.*;// es para poder usar todas las funciones de la biblioteca util
 
 public class nAleatorio {
   public static void main(String[] args){
