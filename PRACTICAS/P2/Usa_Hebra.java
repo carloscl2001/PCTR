3e7fcb57a33080d69bc22e15231f40107a3c5d33
carloscl2001 <carloscorteslora@gmail.com>
2022-10-18 15:08:30 +0200

    hh

--- PRACTICAS/P2/Usa_Hebra.java
+++ PRACTICAS/P2/Usa_Hebra.java
@@ -4,6 +4,7 @@ public class Usa_Hebra {
     public static void main(String[] args)
     throws Exception
     {
+        /*
         hebra p = new hebra(10000 , 0) ;
         hebra q = new hebra(10000 , 1) ;
         p.start() ;
@@ -11,6 +12,7 @@ public class Usa_Hebra {
         p.join() ;
         q.join () ;
         System.out.println(hebra.n);
+        */
     
     }
 }
