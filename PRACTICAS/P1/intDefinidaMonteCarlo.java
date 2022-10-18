ac1aef3e22913e44edff938989a2677958e383fb
carloscl2001 <carloscorteslora@gmail.com>
2022-10-18 13:45:18 +0200

    jj

---
+++ PRACTICAS/P1/intDefinidaMonteCarlo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * @param h 
+ * @return puntos
+ * 
+ */
+
+public class intDefinidaMonteCarlo{
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+
+
+        System.out.print("Introduce el numero de puntos-> ");
+        int puntos = s.nextInt();
+    }
+}
+                        
