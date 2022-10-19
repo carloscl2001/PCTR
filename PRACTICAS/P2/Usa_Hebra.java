893f7bf3cd1228a1036b4f2df703ade06160d7f5
carloscl2001 <carloscorteslora@gmail.com>
2022-10-19 19:32:29 +0200

    fff

--- PRACTICAS/P2/Usa_Hebra.java
+++ PRACTICAS/P2/Usa_Hebra.java
@@ -2,18 +2,17 @@ package PRACTICAS.P2;
 
 import java.util.*;
 
-public class Usa_hebra {
-    public static void main(String[] args)
-        throws Exception
+public class Usa_hebra{
+    public static void main(String[] args) throws Exception
     {
         
-        hebra p = new hebra(10000 , 0) ;
-        hebra q = new hebra(10000 , 1) ;
-        p.start() ;
-        q.start() ;
-        p.join() ;
-        q.join () ;
-        System.out.println(hebra.n);
+        hebra h1 = new hebra(10000 , 0) ;
+        hebra h2 = new hebra(10000 , 1) ;
+        h1.start() ;
+        h2.start() ;
+        h1.join() ;
+        h2.join () ;
+        System.out.println(hebra.n());
     
     
     }
