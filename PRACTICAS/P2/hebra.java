33dd150f79876458abb6be171466f08c442e6977
carloscl2001 <carloscorteslora@gmail.com>
2022-10-19 19:45:03 +0200

    fff

--- PRACTICAS/P2/hebra.java
+++ PRACTICAS/P2/hebra.java
@@ -6,17 +6,28 @@ import java.util.*;
  */
 
 public class hebra extends Thread{
+
     private int tipoHilo ;
     public static int n = 0; // variable de clase
     private int nVueltas ;
     
+
+    /**
+     * Constructor de una hebra
+     * @param nVueltas 
+     * @param tipoHilo
+     * @return crea una hebra
+     */
     public hebra(int nVueltas, int tipoHilo)
     { 
-        this.nVueltas = nVueltas ; 
-        this.tipoHilo = tipoHilo ;
+        this.nVueltas = nVueltas; 
+        this.tipoHilo = tipoHilo;
     }
     
-    public void run () {
+    /**
+     * Método que aumenta y decrementa n la misma cantidad según el hilo
+     */
+    public void run() {
         switch ( tipoHilo ) {
         case 0: for(int i =0; i < nVueltas ; i ++) n++; break ;
         case 1: for(int i =0; i < nVueltas ; i ++) n--; break ;
