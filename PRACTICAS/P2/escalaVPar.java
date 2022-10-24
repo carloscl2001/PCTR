402e6f37361243e18a5a0b7cea2525428c6ddaa5
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-10-24 10:21:17 +0200

    fffff

--- PRACTICAS/P2/escalaVPar.java
+++ PRACTICAS/P2/escalaVPar.java
@@ -4,11 +4,44 @@ public class escalaVPar {
 
     public static int potencia = 6;
     public static int vector[] = new int[(int)Math.pow(10, potencia)];
-
+    public static int escalar = 2;
     public static void main(String[] args) throws Exception{
         //Rellenamos el vector
         for (int i = 0; i < vector.length; i++) {
             vector[i] =(int) Math.random() * 10;
         }
+
+        hilo h1 = new hilo(vector[0], vector.length/2, vector, escalar);
+        hilo h2 = new hilo(vector.length/2 + 1, vector.length, vector, escalar);
+        h1.start();
+        h2.start();
+        h1.join();
+        h2.join();
     }
+}
+
+
+/**
+ * InnerescalaVPar
+ */
+class hilo extends Thread {
+    public final int ini;
+    public final int fin;
+    public int[] vec;
+    public final int esc;
+
+    public hilo(int i, int f, int[]v, int e){
+        ini = i;
+        fin = f;
+        vec = v;
+        esc = e;
+    }
+
+    public void run()
+    {
+        for (int i = ini; i < fin; i++) {
+            vec[i] *= esc;
+        }
+    }
+
 }
