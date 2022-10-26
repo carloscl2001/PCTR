cbed214fd1b49fdfffa051c664d22fc23eb3a442
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-10-26 17:51:43 +0200

    jjj

--- PRACTICAS/P3/ej3/prodEscalar.java
+++ PRACTICAS/P3/ej3/prodEscalar.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * prodEscalar
  * @author Carlos  Antonio Cortés Lora
@@ -5,71 +6,82 @@
  */    
 public class prodEscalar{
     //Declaración de variables
-    public static final int potencia = 6;
-    public static int[] vector1 = new int[(int)Math.pow(10, potencia)];
-    public static int[] vector2 = new int[(int)Math.pow(10, potencia)];
-    public static int[] resultado = new int[(int)Math.pow(10, potencia)];
+    public static final int potencia = 1;
+    public static int[] vector1 = new int[(int)Math.pow(5, potencia)];
+    public static int[] vector2 = new int[(int)Math.pow(5, potencia)];
+    public static int[] resultado = new int[(int)Math.pow(5, potencia)];
 
     /**
-     * Función para rellenar los vectores de lectura con numeros aleatorios
+     * Funcion para rellenar dos vectores de forma aleatoria
+     * @param v1
+     * @param v2
      */
-    public static void rellenarVectores(){
+    public static void rellenarVectores(int v1[], int v2[]){
         for (int i = 0; i < vector1.length; i++) {
-            vector1[i] = (int)(Math.random()*10);
-            vector2[i] = (int)(Math.random()*10);
+            v1[i] = (int)(Math.random()*10);
+            v2[i] = (int)(Math.random()*10);
         }
     }
 
     /**
-     * Función para calcular el producto escalar de los vectores y almacenarlo en el vector resultado
+     * Funcion para multiplicar dos vectores y almacenarlos en v[]
+     * @param v vector donde se almacena el resultado
+     * @param v1
+     * @param v2
      */
-    public static void productoEscalar(){
-        for (int i = 0; i < vector1.length; i++) {
-            resultado[i] = vector1[i] * vector2[i];
+    public static void productoEscalar(int v[], int v1[], int v2[]){
+        for (int i = 0; i < v1.length; i++) {
+            v[i] = v1[i] * v2[i];
         }
     }
 
 
+
+
     /**
-     * Función para mostrar los vectores de lectura y escritura
+     * Funcion para calcular la suma de los elementos del vector resultado
+     * @param v vector donde se almacena el resultado
+     * @return suma -> producto escalar
      */
-    public static void mostrarVectores(){
+    public static int productoEsc(int v[]){
+        int sol = 0;
+        for (int i = 0; i < resultado.length; i++) {
+            sol += v[i];
+        }
+        return sol;
+    }
+
+    /**
+     * Funcion para imprimir los vectores
+     * @param v1
+     * @param v2
+     * @param v
+     */
+    public static void mostrarVectores(int v1[], int v2[], int v[]){
         System.out.println("Vector 1: ");
         for (int i = 0; i < vector1.length; i++) {
-            System.out.print(vector1[i] + " ");
+            System.out.print(v1[i] + " ");
         }
         System.out.println();
         System.out.println("Vector 2: ");
         for (int i = 0; i < vector2.length; i++) {
-            System.out.print(vector2[i] + " ");
+            System.out.print(v2[i] + " ");
         }
         System.out.println();
         System.out.println("Vector Resultante: ");
         for (int i = 0; i < resultado.length; i++) {
-            System.out.print(resultado[i] + " ");
+            System.out.print(v[i] + " ");
         }
         System.out.println();
-    }
-
-    /**
-     * Funcion para calcular la suma de los elementos del vector resultado
-     * @return suma -> producto escalar
-     */
-    public static int productoEsc(){
-        int sol = 0;
-        for (int i = 0; i < resultado.length; i++) {
-            sol += resultado[i];
-        }
-        return sol;
     }
 
     /**
      * Main del prodEscalar
      */
     public static void main(String[] args) {
-        rellenarVectores();
-        productoEscalar();
-        mostrarVectores();
-        System.out.println("La suma de los elementos del vector resultado es: " + productoEsc());
+        rellenarVectores(vector1, vector2);
+        productoEscalar(resultado,vector1, vector2);
+        mostrarVectores(vector1, vector2, resultado);
+        System.out.println("El producto escalar es: " + productoEsc(resultado));
     }
 }
