a03be74e3810e8e1c268ef714054097046be7062
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-11-16 15:32:06 +0100

    jjj

--- PRACTICAS/P5/ej5/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-static boolean esPerfecto(long n) {
-        long suma = 0;
-
-        for (long i = 1; i < n; ++i){if (n % i == 0) suma += i;}
-            
-        if (n == suma){return (true);}
-        else{return (false);}
-    }
