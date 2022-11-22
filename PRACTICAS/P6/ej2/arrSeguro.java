47392bcbc18be0a18586fad186fab9457afcad8a
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2022-11-22 19:14:02 +0100

    hhh

--- PRACTICAS/P6/ej2/arrSeguro.java
+++ PRACTICAS/P6/ej2/arrSeguro.java
@@ -1,3 +1,18 @@
 public class arrSeguro extends Thread{
+
+    /**
+     * Variable del tamaño del array
+     */
+    public static final int N = 1000;
     
+    /** 
+     * Array de enteros
+     */
+    public static int[] array = new int[N];
+
+    public static void main(String[] args) {
+        
+    }    
+
+
 }
--- PRACTICAS/P6/ej5/integCallable.java
+++ PRACTICAS/P6/ej5/integCallable.java
@@ -1,3 +1,20 @@
+import java.util.concurrent.Callable;
+
 public class integCallable {
+
+    /**
+     * Variable estatic que representa el numero de tarreas
+     */
+    public static final int nTareas = Runtime.getRuntime().availableProcessors();
+    public static void main(String[] args) throws Exception{
+
+        //Variable que define el numero de puntos a generar
+        int nPuntos = Integer.parseInt(args[0]);
+
+        //Variable que define el tmaño de la ventana
+        int tVentana = nPuntos/nTareas;
+
+        Arra
+    }
     
 }
