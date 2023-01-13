d1129ff04c1444de27d59eb8ba19dffa67b6a2e4
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-01-13 15:37:21 +0100

    jjjj

--- PRACTICAS/P11/cBonoLoto.java
+++ PRACTICAS/P11/cBonoLoto.java
@@ -2,22 +2,29 @@ import java.rmi.*;
 import java.rmi.registry.*;
 import java.util.Random;
 
+/**
+ * Clase para la implemntacion del cliente del bonoloto
+ * @author Carlos Antonio Cortés Lora 
+ */
 public class cBonoLoto {
     public static void main(String[] args) throws Exception {
         
+        //Vector que contiene la apuesta
         int[] apuesta = new int[6];
 
-        for (int i = 0; i < apuesta.length; ++i) {
+        //Se genera la apuesta
+        for (int i = 0; i < apuesta.length; ++i) 
             apuesta[i] = (int) (Math.random() * 49 + 1);
-        }
-
+        
+        //Se busca el objeto remoto
         iBonoLoto RefObRemoto = (iBonoLoto)Naming.lookup("//localhost/Servidor");
         
-        if(RefObRemoto.compApuesta(apuesta)) {
+        //Se compara la apuesta en la loteria
+        if(RefObRemoto.compApuesta(apuesta)) 
             System.out.println("Has ganado");
-        } else {
+        else 
             System.out.println("Has perdido");
-        }
+        
         
     }
 }
--- PRACTICAS/P11/sBonoLoto.java
+++ PRACTICAS/P11/sBonoLoto.java
@@ -4,29 +4,58 @@ import java.rmi.registry.*;
 import java.net.*;
 import java.util.Random;
 
+/**
+ * Clase para la implemntacion del servidor del bonoloto
+ * @author Carlos Antonio Cortés Lora 
+ */
 public class sBonoLoto extends UnicastRemoteObject implements iBonoLoto{
+
+    /**
+     * Vector con los numeros de la loteria
+     */
     public int[] loteria = new int[6];
    
+    /**
+     * Constructor de la clase
+     * @throws RemoteException
+     */
     public sBonoLoto() throws RemoteException {
         resetServidor();
     }
 
-    public void resetServidor(){
-        for (int i = 0; i < loteria.length; ++i) {
+    /**
+     * Metodo para reiniciar el servidor
+     * @throws RemoteException
+     */
+    public void resetServidor() throws RemoteException{
+        for (int i = 0; i < loteria.length; ++i) 
             loteria[i] = (int) (Math.random() * 49 + 1);
-        }
+        
     }
 
+    /**
+     * Metodo para comparar la apuesta con la loteria
+     * @return true si la apuesta es correcta, false en caso contrario
+     */
     public boolean compApuesta(int[] apuesta) throws RemoteException {   
         boolean ganador = true;
-		for( int i = 0; i < loteria.length; ++i ){
+		for( int i = 0; i < loteria.length; ++i )
             if( loteria[i] != apuesta[i] ) ganador = false;
-        }
+        
 		return ganador;
     } 
 
+    /**
+     * Main del servidor
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+
+        //creamos el objeto remoto
         iBonoLoto obj = new sBonoLoto();
+
+        //Se registra el servicio
         Naming.bind("Servidor", obj);
         System.out.println("Servidor BonoLoto preparado");
     }
